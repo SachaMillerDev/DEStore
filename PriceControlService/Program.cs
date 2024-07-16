414ac4e193d95bc95ded32cb0e02628b22a7a506
Sacha Miller <SachaM@forrit.com>
2024-07-16 12:12:23 +0100

    updates

--- PriceControlService/Program.cs
+++ PriceControlService/Program.cs
@@ -3,12 +3,14 @@ using PriceControlService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddDbContext<PriceControlContext>(options =>
     options.UseInMemoryDatabase("PriceControlDB"));
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
